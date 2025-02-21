d1bf0b5c9e7118bc409ee32acab871c127c78d4c
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2025-02-21 18:50:42 +0100

    Feat: Unit Test

--- Backend/src/card-profile/card-profile.service.spec.ts
+++ Backend/src/card-profile/card-profile.service.spec.ts
@@ -14,6 +14,7 @@ describe('CardProfileService', () => {
         id: 'fee123',
         ...data.data,
       })),
+      findFirst: jest.fn().mockResolvedValue(null),
       findMany: jest.fn().mockResolvedValue([
         {
           id: 'fee1',
