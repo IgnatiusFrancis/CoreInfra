0b960f2b235fd8d63b4e54731b18f976f4c32511
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2025-02-23 19:10:20 +0100

    Feat: Cors error fix

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -9,7 +9,7 @@ import { setupSwagger } from './utils';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://core-infra-psi.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,PATCH',
     allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With',
     credentials: true,
