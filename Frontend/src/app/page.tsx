6465b2513e3d1634c9fab0d553e126fff27d3898
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2025-02-23 19:12:47 +0100

    Feat: Dashboard error

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,13 +1,8 @@
-
-import CardProfile from "./card-profile/page";
-import CardRequest from "./card-request/page";
 import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
     <main>
-      <CardProfile />
-      <CardRequest />
       <Dashboard />
     </main>
   );
